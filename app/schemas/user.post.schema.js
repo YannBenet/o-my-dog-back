20650952ad92baf3b68e43634ad966209f750674
JeremyCharles8 <sowuf88@gmail.com>
2024-07-01 14:50:27 +0200

    feat: :sparkles: add users register functionality and fix some database issues

---
+++ app/schemas/user.post.schema.js
@@ -0,0 +1,25 @@
+import Joi from 'joi';
+
+export default Joi.object({
+  firstname: Joi.string()
+    .alphanum()
+    .min(3)
+    .max(30),
+  lastname: Joi.string()
+    .alphanum()
+    .min(3)
+    .max(30),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: false } }),
+  password: Joi.string().min(8).max(64)
+    .pattern(new RegEXP('^(?=.*[a-z].*[a-z])(?=.*[A-Z].*[A-Z])(?=.*\d.*\d)(?=.*[@$!%*?&].*[@$!%*?&])[\w@$!%*?&]$')
+    )
+    .message('Password must contain at least two lowercase letters, two uppercase letters, two numbers and two special characters'),
+  repeatPassword: Joi.string()
+    .valid(Joi.ref('password'))
+    .message({ 'any.only': 'Passwords must be the same' }),
+  city: Joi.string().min(2),
+  phoneNumber: Joi.string().max(13)
+    .pattern(new RegExp('^[0-9+]+$'))
+    .message('Incorrect phone number'),
+});
