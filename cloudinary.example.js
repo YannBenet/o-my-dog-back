94469f7210f5fa52d97ed6a41a3b43396b338628
Pierre Guillard <pguillard95@gmail.com>
2024-07-03 15:23:05 +0200

    feat: :lock: Adding CORS for all the road with the front address. Adding the method to post img for the front.

--- cloudinary.example.js
+++ cloudinary.example.js
@@ -33,4 +33,39 @@ export async function uploadImage (imagePath) {
     console.log(imgUrl)
 
 
+// ? Pour React 
+
+
+// import { v2 as cloudinary } from 'cloudinary';
+
+
+// async function uploadImage (file) {
+//   cloudinary.config({ 
+//     cloud_name: 'dfniqh3lp', 
+//     api_key: '513255815138222', 
+//     api_secret: process.env.CLDAPISECRET
+//   });
+
+//   const options = {
+//     folder : "userImages",
+//     use_filename: true,
+//     unique_filename: true,
+//     overwrite: false,
+//     transformation : [
+//     { width: 250, height: 250, crop:"auto" },
+//     { quality: 'auto' },
+//     { fetch_format: "auto" }
+//   ]
+//   };
+
+//   // Upload an image
+//   const uploadResult = await cloudinary.uploader
+//   .upload( file, options)
+//   .catch((error) => {
+//       console.log(error);
+//   });
+
+//   console.log(uploadResult);
+
+// }
 
