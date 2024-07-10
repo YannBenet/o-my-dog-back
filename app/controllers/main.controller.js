fd66845cc2334aa5a0d83f0f124d0f61e875c0c7
JeremyCharles8 <sowuf88@gmail.com>
2024-07-10 18:52:45 +0200

    fix: :bug: Update joi schema and modify animal findAll request

--- app/controllers/main.controller.js
+++ app/controllers/main.controller.js
@@ -1,5 +1,5 @@
-export default {
-    index(req, res) {
-        res.sendFile('index.html');
-    }
-};
+// export default {
+//     index(req, res) {
+//         res.sendFile('index.html');
+//     }
+// };
--- app/io.app.js
+++ app/io.app.js
@@ -1,13 +1,44 @@
-export default {
-  init(server){
-    this.server = server;
-    this.listenEvent();
-  },
+// export default {
+//   init(server){
+//     this.server = server;
+//     this.listenEvent();
+//     this.users = [];
+//   },
 
-  listenEvent(){
-    this.server.on('connection', (socket) => {
-      console.log(`Utilisateur connecté: ${socket.id}`);
-    });
-  }
+//   listenEvent(){
+//     // user connexion to websocket server
+//     this.server.on('connection', (socket) => {
+//       const user = {
+//         id: socket.id
+//       };
+//       this.users.push(user);
+//       console.log(this.users);
 
-}
+//       // user join room with an other user
+//       // socket.on('joinRoom', (users) => {
+
+//       //   const userIsPresent = this.server.sockets.sockets.get(users.idA)
+//       //   console.log(userIsPresent);
+//       //   const room = `${users.idA} - ${users.idB}`;
+//       //   console.log(room);
+//       //   socket.join(room)
+//       //   console.log(`${users.idA} et ${users.idB} ont rejoint le salon`);
+//       // });
+
+//       // // users in the same room can send and receive messages
+//       // socket.on('sendMessage',({ message, room }) => {
+//       //   console.log(`message: ${message} / salle: ${room}`);
+//       //   this.server.to(room).emit('receiveMessage', { message, clientId: socket.id });
+//       // })
+
+//       socket.on('disconnect', () => {
+//         console.log(`user ${user.id} disconnected`);
+//         this.users.forEach((user) => {
+//           if(user.id === socket.id){
+//             this.users.delete(user);
+//           }
+//         })
+//       })
+//     });
+//   }
+// }
