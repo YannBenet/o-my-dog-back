5fd34ebebcb397f0d940112b46538c9e2eb7d2f7
Pierre Guillard <pguillard95@gmail.com>
2024-07-11 15:52:01 +0200

    New pg test

--- app/config/pg.client.js
+++ app/config/pg.client.js
@@ -8,7 +8,17 @@ if (process.env.NODE_ENV === 'development') {
     client = new pg.Client(process.env.PGURL_DEV)
 } else if (process.env.NODE_ENV === 'production') {
     console.log("connexion à la bdd en ligne");
-     client = new pg.Client(process.env.PGURL_PROD)
+    console.log(process.env.PGURL_PROD);
+    client = new pg.Client({
+        host: process.env.PGHOST,
+        database: process.env.PGDATABASE,
+        user: process.env.PGUSER,
+        port: process.env.PGPORT,
+        password: process.env.PGPASSWORD,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    });
 }
 
 await client.connect()
