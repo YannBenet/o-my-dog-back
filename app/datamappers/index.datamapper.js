bef300879dbb6e7447e4c03cceb4096e176d5512
Pierre Guillard <pguillard95@gmail.com>
2024-07-11 14:34:07 +0200

    heroku test - need to be undone

--- app/datamappers/index.datamapper.js
+++ app/datamappers/index.datamapper.js
@@ -1,12 +1,12 @@
-import client from "../config/pg.client.js";
+// import client from "../config/pg.client.js";
 
 import UserDatamapper from './user.datamapper.js';
 import AnnouncementDatamapper from "./announcement.datamapper.js";
 import AnimalDatamapper from './animal.datamapper.js';
 
-UserDatamapper.init({client});
-AnnouncementDatamapper.init({client});
-AnimalDatamapper.init({client});
+// UserDatamapper.init({client});
+// AnnouncementDatamapper.init({client});
+// AnimalDatamapper.init({client});
 
 export {
   UserDatamapper,
