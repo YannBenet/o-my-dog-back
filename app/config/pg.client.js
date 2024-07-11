c45448bc0772acf3b045b27ac13bcd8627c49468
Pierre Guillard <pguillard95@gmail.com>
2024-07-11 15:38:17 +0200

    Test heroku DB

--- app/config/pg.client.js
+++ app/config/pg.client.js
@@ -1,8 +1,16 @@
 import pg from 'pg';
 import 'dotenv/config';
 
-const client = new pg.Client(process.env.PGURL);
+let client; 
+
+if (process.env.NODE_ENV === 'development') {
+    console.log("connexion en local");
+    client = new pg.Client(process.env.PGURL_DEV)
+} else if (process.env.NODE_ENV === 'production') {
+    console.log("connexion à la bdd en ligne");
+     client = new pg.Client(process.env.PGURL_PROD)
+}
+
 await client.connect()
 
-export default client; 
-
+export default client;
