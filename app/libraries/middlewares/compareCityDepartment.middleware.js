5e24de0b3f84ac6551d831e417154c1c5fed7c8a
YannB <yannbenet@gmail.com>
2024-07-16 11:57:57 +0200

    perf: :zap: Improve req to api on compareCityDepartment middleware

--- app/libraries/middlewares/compareCityDepartment.middleware.js
+++ app/libraries/middlewares/compareCityDepartment.middleware.js
@@ -8,39 +8,35 @@ export default () => async (req, res, next) => {
   if (req.body.city) {
     const cityReq = req.body.city.split(' ')[0];
     const departmentReq = req.body.department_label;
-    const departmentCodeReq = req.body.department_code;
 
     try {
       const cityResult = await fetch(`https://geo.api.gouv.fr/communes?nom=${cityReq}&fields=nom,departement&boost=population&limit=5`);
       const dataCity = await cityResult.json();
-      const departmentResult = await fetch(`https://geo.api.gouv.fr/departements/${departmentCodeReq}`);
-      const dataDepartment = await departmentResult.json();
-      if(!dataCity.length || !dataDepartment) {
+      const departmentFound = dataCity.find((city) => city.departement.nom === departmentReq);
+      const departmentSelected = departmentFound.departement.nom;
+
+
+      console.log("Résultat de departmentSelected", departmentSelected);
+      if(!dataCity.length || !departmentFound) {
           console.log('No data');
           throw new ApiError('Invalid city name or no exact match found.', { status: 400 });
-     }
-
-      const cityApi = dataCity[0].nom;
-      const departmentNameApi = dataDepartment.nom;
-      const departmentCodeApi = dataDepartment.code;
+     } 
+      const cityFound = dataCity.find((city) => city.nom === cityReq);
+      const citySelected = cityFound.nom;
+      console.log("Résultat du citySelected", citySelected);
 
       // Compare city with same writing format
       function normalizeString(str) {
           return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
       }
 
-      // id most of 1 city found, and the first of table is the right one
-      if (dataCity.length > 1 && normalizeString(cityApi) === normalizeString(cityReq) && departmentCodeApi === departmentCodeReq) {
-          req.body.city = cityApi;
-          req.body.department_label = departmentNameApi;
+      // if city selected is the right one, write it the same way in params
+      if (normalizeString(citySelected) === normalizeString(cityReq) && normalizeString(departmentSelected) === normalizeString(departmentReq)) {
+          req.body.city = citySelected;
+          req.body.department_label = departmentSelected;
           return next();
       } 
-      // if one city is found and is right
-      else if (dataCity.length === 1 && normalizeString(cityApi) === normalizeString(cityReq && departmentCodeApi === departmentCodeReq)) {
-          req.body.city = cityApi;
-          req.body.department_label = departmentNameApi;
-          return next();
-      }
+
       else {
           throw new ApiError('Invalid city name or no exact match found.', { status: 400 });
       }   
